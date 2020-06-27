739fdb3af299cc27db17e15dd7a374f0cca180fd
Hameed <hdrhmd@amazon.com>
2020-06-27 03:30:31 -0700

    rearranged the code

---
+++ scripts/content.js
@@ -0,0 +1,13 @@
+const blacklist = [
+    "facebook.com",
+    "youtube.com",
+    "twitter.com"
+];
+
+$(document).ready(() => {
+    if (blacklist.filter(w => window.location.origin.includes(w)).length > 0) {
+        window.location.href = chrome.extension.getURL("/pages/blocked.html");
+    }
+    const src = chrome.extension.getURL("/pages/iframe/index.html");
+    $('body').prepend(`<iframe src="${src}" width="100%" height="10px" frameBorder="0px"></iframe>`);
+});
